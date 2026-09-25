588dcafc74602fd9895bdbb74dca334d11a4955b
sgiungla <salvo_giunta@hotmail.it>
2026-09-25 17:10:47 +0200

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -1,4 +1,4 @@
 export const DEFAULT_CLOUD_CONFIG = {
   supabaseUrl: 'https://eqrcdvhhfrkpmnzqoerr.supabase.co',
-  supabasePublishableKey: ''
+  supabasePublishableKey: 'sb_publishable_FuVed9JVRrVcyyl0GIxVkQ_1Yboz1Gi'
 };
