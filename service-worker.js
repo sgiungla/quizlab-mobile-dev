4aacc71b0ac19d5db28c1b032cae93acd4b06f88
sgiungla <salvo_giunta@hotmail.it>
2026-09-25 15:18:47 +0200

    Bump cache for sync badge

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='quizlab-mobile-dev-v0.4.2';
+const CACHE='quizlab-mobile-dev-v0.4.3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./db.js','./sync-adapter.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
